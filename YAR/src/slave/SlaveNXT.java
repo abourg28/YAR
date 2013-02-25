b1152930e1e3f7853696828aca624873230d099c
Alex Bourgeois <abourg28@gmail.com>
2013-02-25 09:32:18 -0500

    Added classes containing mains

---
+++ YAR/src/slave/SlaveNXT.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package slave;
+
+/**
+ * @author alex
+ *
+ */
+public class SlaveNXT {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

dfa77e1bce89ec817f9268963f655c891a5e56b1
Alex Bourgeois <abourg28@gmail.com>
2013-03-18 16:01:22 -0400

    Replaced localization with Minhs version and implemented a few methods in YARRobot

--- YAR/src/master/USLocalizer.java
+++ YAR/src/master/USLocalizer.java
@@ -25,170 +25,231 @@ public class USLocalizer {
 		FALLING_EDGE, RISING_EDGE
 	};
 
-	public static double ROTATION_SPEED = 30;
+	public static int ROTATION_SPEED = 30;
 	public static int WALL_DIST = 40;
 	public static int NOISE = 1;
 	public static int TRESH_HOLD = 7;
+	private final int FILTER_OUT = 10;
 
 	private Odometer odo;
 	private IRobot robot;
 	private UltrasonicSensor us;
 	private LocalizationType locType;
 	private INavigator nav;
+	private int correctDistance;
+	private int distanceWant;
+	private int filterControl;
+	private int distance;
 
 	/** The number of times a wall is detected. */
 	private int visibleCount = 0;
 	/** The number of times a wall is not detected. */
 	private int notVisibleCount = 0;
 
-	public USLocalizer(Odometer odo, UltrasonicSensor us, LocalizationType locType) {
+	public USLocalizer(Odometer odo, UltrasonicSensor us,
+			LocalizationType locType) {
 		this.odo = odo;
 		this.robot = odo.getRobot();
 		this.locType = locType;
 		this.nav = odo.getNavigator();
 		this.us = us;
+		filterControl = 0;
 	}
 
 	public void doLocalization() {
 		double[] pos = new double[3];
-		double[] noisePos = new double[3];
-		double angle, alphaAngle, betaAngle;
+		double[] pos2 = new double[3];
+		double angleA, angleB;
 
 		if (locType == LocalizationType.FALLING_EDGE) {
-			LCD.drawString("Falling Edge!", 0, 5);
-			// Rotate until the wall is not visible
-			while (getFilteredData() <= WALL_DIST || notVisibleCount < 5) {
-				robot.setRotationSpeed(ROTATION_SPEED);
-			}
+			correctDistance = getFilteredData();
 
-			// Rotate until the wall is visible
-			while (getFilteredData() > WALL_DIST + NOISE || visibleCount < 5) {
-				robot.setRotationSpeed(ROTATION_SPEED);
+			int firstWall = 1; // 1 corresponds to seeing a wall and 0
+								// corresponds to no wall
+
+			robot.getLeftMotor().setSpeed(ROTATION_SPEED);// starts rotating
+			robot.getRightMotor().setSpeed(-ROTATION_SPEED);
+			robot.getLeftMotor().forward();
+			robot.getRightMotor().backward();
+			while (firstWall == 1) {
+
+				if (correctDistance - distanceWant > 2) // falls out of noise
+														// margin sees no wall
+				{
+					firstWall = firstWall - 1; // exit
+				}
+				correctDistance = getFilteredData();// re-update the distance
 			}
-			odo.getPosition(noisePos);
-			while (getFilteredData() >= WALL_DIST - NOISE || visibleCount < 5) {
-				robot.setRotationSpeed(ROTATION_SPEED);
+			// Sound.playTone(1200,150);
+			// robot is now not facing any wall.
+			LCD.drawString("US: " + us.getDistance(), 0, 4);
+			while (getFilteredData() < 64) {
 			}
-			// Record alpha angle
+			while (getFilteredData() > 62) {
+			}
 			Sound.beep();
-			odo.getPosition(pos);
-			alphaAngle = (pos[2] + noisePos[2]) / 2;
-
-			// Rotate in other direction until the wall is not visible
-			while (getFilteredData() < WALL_DIST || notVisibleCount < 5) {
-				robot.setRotationSpeed(-ROTATION_SPEED);
-			}
-
-			// Rotate until the wall is visible
-			while (getFilteredData() > WALL_DIST + NOISE || visibleCount < 5) {
-				robot.setRotationSpeed(-ROTATION_SPEED);
-			}
-			odo.getPosition(noisePos);
-			while (getFilteredData() >= WALL_DIST - NOISE || visibleCount < 5) {
-				robot.setRotationSpeed(-ROTATION_SPEED);
-			}
-			// Record beta angle
-			Sound.beep();
-			odo.getPosition(pos);
-			betaAngle = (pos[2] + noisePos[2]) / 2;
-
-			if (alphaAngle > betaAngle) {
-				angle = 45 - (alphaAngle + betaAngle) / 2;
-			} else {
-				angle = 225 - (alphaAngle + betaAngle) / 2;
-			}
-
-			// Update the odometer
-			odo.setPosition(new double[] { 0.0, 0.0, betaAngle + angle },
-					new boolean[] { true, true, true });
-			nav.turnTo(0.0);
-			odo.setPosition(new double[] { 0.0, 0.0, 0.0 }, new boolean[] {
-					true, true, true });
-			robot.setRotationSpeed(0);
-			Sound.buzz();
-			try {
-				Thread.sleep(2000);
+			robot.getLeftMotor().stop();
+			robot.getRightMotor().stop();
+			try {// rest for 300 ms
+				Thread.sleep(300);
 			} catch (InterruptedException e) {
+				Sound.buzz();
 			}
+			angleA = odo.getTheta();
+			robot.getRightMotor().forward();
+			robot.getLeftMotor().backward();
+			robot.getLeftMotor().setSpeed(-ROTATION_SPEED);// starts rotating
+															// the other way
+			robot.getRightMotor().setSpeed(ROTATION_SPEED);
 
+			// switch direction and wait until it sees no wall
+			// keep rotating until the robot sees a wall, then latch the angle
+			while (getFilteredData() < 64) {
+			}
+			while (getFilteredData() > 62) {
+			}
+			Sound.playTone(1200, 150);
+
+			angleB = odo.getTheta();
+
+			robot.getLeftMotor().setSpeed(0);
+			robot.getRightMotor().setSpeed(0);
+			try {// rest for 300 ms
+				Thread.sleep(300);
+			} catch (InterruptedException e) {
+				Sound.buzz();
+			}
+			// angleA is clockwise from angleB, so assume the average of the
+			// angles to the right of angleB is 45 degrees past 'north'
+
+			double offset = -6.9;
+			if (angleA > angleB) {
+
+				LCD.clear();
+				LCD.drawString("" + (((angleA + angleB) / 2) + 135), 0, 6);
+				robot.getLeftMotor().setSpeed(0);
+				robot.getRightMotor().setSpeed(0);
+
+				nav.turnTo(360
+						- Math.abs((360 - angleB)
+								- Math.abs(45 - (angleA + (360 - angleB)) / 2))
+						- 180 + offset);
+
+				Sound.playTone(1200, 150);
+
+				odo.setX(0);
+				odo.setY(0);
+				odo.setTheta(90);
+			} else {
+				LCD.clear();
+				robot.getLeftMotor().setSpeed(0);
+				robot.getRightMotor().setSpeed(0);
+				nav.turnTo(offset
+						+ 360
+						- Math.abs((360 - angleB)
+								- Math.abs(45 - (angleA + (360 - angleB)) / 2)));
+				Sound.playTone(1200, 150);
+
+				odo.setX(0);
+				odo.setY(0);
+				odo.setTheta(90);
+			}
 		} else {
-			/*
-			 * The robot should turn until it sees the wall, then look for the
-			 * "rising edges:" the points where it no longer sees the wall. This
-			 * is very similar to the FALLING_EDGE routine, but the robot will
-			 * face toward the wall for most of it.
-			 */
-
-			while (getFilteredData() > WALL_DIST - TRESH_HOLD) {
-				robot.setRotationSpeed(ROTATION_SPEED);
+			int firstWall = 0; // asumes robot doesnt face the wall
+			robot.getLeftMotor().setSpeed(ROTATION_SPEED);// starts rotating
+			robot.getRightMotor().setSpeed(-ROTATION_SPEED);
+			robot.getLeftMotor().forward();
+			robot.getRightMotor().backward();
+			while (firstWall == 0) {
+				correctDistance = getFilteredData(); // re-update the distance
+				if (correctDistance - distanceWant < -2) // falls out of noise
+															// margin sees wall
+				{
+					firstWall = firstWall - 1; // exit
+				}
+			}
+			// keep rotating until the robot sees no wall, then latch the angle
+			while (getFilteredData() > 58) {
+			}
+			while (getFilteredData() < 60) {
+			}
+			double angleA1 = odo.getTheta();
+			robot.getLeftMotor().stop();
+			robot.getRightMotor().stop();
+			robot.getLeftMotor().setSpeed(ROTATION_SPEED);
+			robot.getRightMotor().setSpeed(ROTATION_SPEED);
+			robot.getLeftMotor().backward();
+			robot.getRightMotor().forward();
+			while (getFilteredData() > 58) {
+			}
+			while (getFilteredData() < 60) {
 			}
 
-			// Rotate until the wall isn't visible
-			while (getFilteredData() <= WALL_DIST - NOISE) {
-				robot.setRotationSpeed(ROTATION_SPEED);
-			}
-			Sound.beep();
-			odo.getPosition(noisePos);
-			betaAngle = (pos[2] + noisePos[2]) / 2;
+			double angleB1 = odo.getTheta();
+			robot.getLeftMotor().setSpeed(0);
+			robot.getRightMotor().setSpeed(0);
+			if (angleA1 > angleB1) {
 
-			// Rotate in other direction until the wall is visible
-			while (getFilteredData() > WALL_DIST - TRESH_HOLD) {
-				robot.setRotationSpeed(-ROTATION_SPEED);
-			}
+				LCD.clear();
+				LCD.drawString("" + (((angleA1 + angleB1) / 2) + 135), 0, 6);
 
-			// Rotate until wall isn't visible anymore
-			while (getFilteredData() < WALL_DIST - NOISE) {
-				robot.setRotationSpeed(-ROTATION_SPEED);
-			}
-			odo.getPosition(noisePos);
-			while (getFilteredData() <= WALL_DIST + NOISE) {
-				robot.setRotationSpeed(-ROTATION_SPEED);
-			}
+				nav.turnTo(360 - Math.abs((360 - angleB1)
+						- Math.abs(45 - (angleA1 + (360 - angleB1)) / 2)) - 180);
 
-			Sound.beep();
-			odo.getPosition(pos);
-			alphaAngle = (noisePos[2] + pos[2]) / 2;
-			if (alphaAngle > betaAngle) {
-				angle = 105 - (alphaAngle + betaAngle) / 2;
+				Sound.playTone(1200, 150);
+				odo.setX(0);
+				odo.setY(0);
+				odo.setTheta(90);
 			} else {
-				angle = 285 - (alphaAngle + betaAngle) / 2;
-			}
+				LCD.clear();
 
-			// Update the odometer
-			odo.setPosition(new double[] { 0.0, 0.0, alphaAngle + angle },
-					new boolean[] { false, false, true });
-			nav.turnTo(0.0);
-			odo.setPosition(new double[] { 0.0, 0.0, 0.0 }, new boolean[] {
-					true, true, true });
-			robot.setRotationSpeed(0);
-			Sound.buzz();
-			try {
-				Thread.sleep(2000);
-			} catch (InterruptedException e) {
+				nav.turnTo(360 - Math.abs((360 - angleB1)
+						- Math.abs(45 - (angleA1 + (360 - angleB1)) / 2)));
+				Sound.playTone(1200, 150);
+
+				odo.setX(0);
+				odo.setY(0);
+				odo.setTheta(90);
 			}
 		}
 	}
 
 	private int getFilteredData() {
-		int distance = 255;
+		boolean filter = true;
+		filterControl = 0;
 
-		// do a ping
-		us.ping();
-		distance = us.getDistance();
+		while (filter) {
 
-		// wait for the ping to complete
-		try {
-			Thread.sleep(10);
-		} catch (InterruptedException e) {
+			// do a ping
+			us.ping();
+			// wait for the ping to complete
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+			}
+
+			distance = us.getDistance();
+
+			if (distance == 255 && (filterControl < FILTER_OUT)) {
+				filterControl++;
+			}
+
+			else {
+				filter = false;
+			}
+
 		}
-		if (distance > WALL_DIST) {
-			notVisibleCount++;
-			visibleCount = 0;
-		} else {
-			visibleCount++;
-			notVisibleCount = 0;
-		}
+
 		return distance;
+	}
+	
+
+	private static int convertAngle(double radius, double width, double angle) {
+		return convertDistance(radius, Math.PI * width * angle / 360.0);
+	}
+
+	private static int convertDistance(double radius, double distance) {
+		return (int) ((180 * distance) / (Math.PI * radius));
 	}
 
 }

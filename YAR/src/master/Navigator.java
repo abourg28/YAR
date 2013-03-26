ddae6eeaabb52a8016605e1491a16011c616f3d1
Alex Bourgeois <abourg28@gmail.com>
2013-03-26 16:05:23 -0400

    Cure for parkinsons

--- YAR/src/master/Navigator.java
+++ YAR/src/master/Navigator.java
@@ -3,21 +3,22 @@ package master;
 import lejos.nxt.LCD;
 import lejos.nxt.Sound;
 
-
 /**
- * This is an abstract navigator. This is where we want to have the default navigator code.
+ * This is an abstract navigator. This is where we want to have the default
+ * navigator code.
+ * 
  * @author alex
- *
+ * 
  */
 public abstract class Navigator extends Thread implements INavigator {
 
 	final static double DEG_ERR = 4.0, CM_ERR = 5.0;
 	final static int FORWARD_SPEED = 40, ROTATION_SPEED = 110;
-	
+
 	protected Odometer odo;
 	protected boolean isNavigating;
 	protected IRobot robot;
-	
+
 	protected Navigator(IRobot robot, Odometer odo) {
 		this.odo = odo;
 		this.robot = robot;
@@ -29,11 +30,11 @@ public abstract class Navigator extends Thread implements INavigator {
 		double minAng = 0;
 		double[] pos = new double[3];
 		this.odo.getPosition(pos);
-		
+
 		double dy = y - pos[1];
 		double dx = x - pos[0];
-//		LCD.drawString("Turning !!    ", 0, 6);
-		
+		// LCD.drawString("Turning !!    ", 0, 6);
+
 		if (dx > 0) {
 			minAng = Math.atan(dy / dx) * (180.0 / Math.PI);
 		} else if (dx < 0 && dy > 0) {
@@ -41,14 +42,14 @@ public abstract class Navigator extends Thread implements INavigator {
 		} else if (dx < 0 && dy < 0) {
 			minAng = (Math.atan(dy / dx) - Math.PI) * (180.0 / Math.PI);
 		}
-		
+
 		// Rotate
 		turnTo(-minAng);
-		
-//		LCD.drawString("Going Forward!!", 0, 6);
+
+		// LCD.drawString("Going Forward!!", 0, 6);
 		this.robot.setForwardSpeed(FORWARD_SPEED);
-		
-		while (Math.abs(x - pos[0]) > CM_ERR || Math.abs(y - pos[1]) > CM_ERR) {	
+
+		while (Math.abs(x - pos[0]) > CM_ERR || Math.abs(y - pos[1]) > CM_ERR) {
 			this.odo.getPosition(pos);
 		}
 		this.robot.setSpeeds(0, 0);
@@ -58,43 +59,48 @@ public abstract class Navigator extends Thread implements INavigator {
 	public void turnTo(double angle) {
 		isNavigating = true;
 		Sound.beep();
-		
+
 		double[] pos = new double[3];
 		this.odo.getPosition(pos);
 		double error = minimizeAngle(angle - pos[2]);
-		
 
-		while (Math.abs(error) > DEG_ERR) {
-//			LCD.drawString("Angle:" + angle + "  ", 0, 4);
-//			LCD.drawString("Error:" + error + "   ", 0, 5);
-			this.odo.getPosition(pos);
-			error = minimizeAngle(angle - pos[2]);
+		// LCD.drawString("Angle:" + angle + "  ", 0, 4);
+		// LCD.drawString("Error:" + error + "   ", 0, 5);
+		this.odo.getPosition(pos);
+		error = minimizeAngle(angle - pos[2]);
 
-			if (error < -180.0) {
-	
-				this.robot.getLeftMotor().setSpeed(ROTATION_SPEED);
-				this.robot.getLeftMotor().backward();
-				this.robot.getRightMotor().setSpeed(ROTATION_SPEED);
-				this.robot.getRightMotor().forward();
-				
-			} else if (error < 0.0) {
-				this.robot.getLeftMotor().setSpeed(ROTATION_SPEED);
-				this.robot.getLeftMotor().forward();
-				this.robot.getRightMotor().setSpeed(ROTATION_SPEED);
-				this.robot.getRightMotor().backward();
-			} else if (error > 180.0) {
-				this.robot.getLeftMotor().setSpeed(ROTATION_SPEED);
-				this.robot.getLeftMotor().backward();
-				this.robot.getRightMotor().setSpeed(ROTATION_SPEED);
-				this.robot.getRightMotor().forward();
-			} else {
-				this.robot.getLeftMotor().setSpeed(ROTATION_SPEED);
-				this.robot.getLeftMotor().forward();
-				this.robot.getRightMotor().setSpeed(ROTATION_SPEED);
-				this.robot.getRightMotor().backward();
-			}
+		if (error < -180.0) {
+
+			this.robot.getLeftMotor().setSpeed(ROTATION_SPEED);
+			this.robot.getLeftMotor().backward();
+			this.robot.getRightMotor().setSpeed(ROTATION_SPEED);
+			this.robot.getRightMotor().forward();
+
+		} else if (error < 0.0) {
+			this.robot.getLeftMotor().setSpeed(ROTATION_SPEED);
+			this.robot.getLeftMotor().forward();
+			this.robot.getRightMotor().setSpeed(ROTATION_SPEED);
+			this.robot.getRightMotor().backward();
+		} else if (error > 180.0) {
+			this.robot.getLeftMotor().setSpeed(ROTATION_SPEED);
+			this.robot.getLeftMotor().backward();
+			this.robot.getRightMotor().setSpeed(ROTATION_SPEED);
+			this.robot.getRightMotor().forward();
+		} else {
+			this.robot.getLeftMotor().setSpeed(ROTATION_SPEED);
+			this.robot.getLeftMotor().forward();
+			this.robot.getRightMotor().setSpeed(ROTATION_SPEED);
+			this.robot.getRightMotor().backward();
 		}
 		
+		while (Math.abs(error) > DEG_ERR) {
+			this.odo.getPosition(pos);
+			error = minimizeAngle(angle - pos[2]);
+		}
+		
+		this.robot.getLeftMotor().setSpeed(0);
+		this.robot.getRightMotor().setSpeed(0);
+
 		this.robot.setSpeeds(0, 0);
 	}
 
@@ -110,15 +116,11 @@ public abstract class Navigator extends Thread implements INavigator {
 		return error;
 	}
 
-		
-
-	
-
 	@Override
 	public boolean isNavigating() {
 		return this.isNavigating;
 	}
-	
+
 	protected static int convertAngle(double radius, double width, double angle) {
 		return convertDistance(radius, width * angle / 2);
 	}
